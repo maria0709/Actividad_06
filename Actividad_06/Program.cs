5bc0ca7526355492f18833f41f3227e449276372
MARIA RENEÉ ROJAS ARCAYNE <88RO94097790@campus.economicas.uba.ar>
2021-03-26 14:28:31 -0300

    Entrega de la actividad 6, a pedido del docente.

---
+++ Actividad_06/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_06
+{
+    class Program
+    {
+        //Lea las teclas presionadas y finalice al presionar Shift+Ctrl+F.
+        static void Main(string[] args)
+
+        { 
+        ConsoleKeyInfo tecla;
+
+            do
+            {
+                tecla = Console.ReadKey();
+                Console.WriteLine();
+                if ((tecla.Key == ConsoleKey.F) &&
+                   ((tecla.Modifiers & ConsoleModifiers.Shift) != 0) &&
+                   ((tecla.Modifiers & ConsoleModifiers.Control) != 0))
+
+                 {
+                    break;
+                 }
+
+            }
+            while (true);
+        }
+    }
+}
+
